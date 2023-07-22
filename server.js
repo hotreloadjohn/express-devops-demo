79ebf21c932f6e990bfeca314581a4a3098194aa
hotreloadjohn <johnng87@protonmail.com>
2023-07-22 22:41:58 +0800

    refactor: Add more products to products endpoint

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.get("/api/products", (req, res) => {
     { id: 1, name: "product 1" },
     { id: 2, name: "product 2" },
     { id: 3, name: "product 3" },
+    { id: 4, name: "product 4" },
+    { id: 5, name: "product 5" },
   ];
   res.json(products);
 });
