537fa7f41f88204806de844a7169e4f0b03fc7de
hotreloadjohn <johnng87@protonmail.com>
2023-07-22 21:20:17 +0800

    feat: Add product endpoint

--- server.js
+++ server.js
@@ -5,5 +5,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!!!>>>>>>");
 });
 
+// GET product api
+app.get("/api/products", (req, res) => {
+  const products = [
+    { id: 1, name: "product 1" },
+    { id: 2, name: "product 2" },
+    { id: 3, name: "product 3" },
+  ];
+  res.json(products);
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
